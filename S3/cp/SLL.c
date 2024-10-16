d0983ea1059c559ced129b917f4075895074d66a
Joshua <joshua.palokaran@gmail.com>
2024-10-16 22:33:29 +0530

    New prgrm

--- S3/cp/SLL.c
+++ S3/cp/SLL.c
@@ -1,4 +1,3 @@
-// Online C compiler to run C program online
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -232,4 +231,4 @@ void main()
 		scanf(" %c",&ch);
 	}
 	while((ch=='y')||(ch=='Y'));
-}
+} 
---
+++ S3/cp/java/misc.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> studentNames = new ArrayList<>();
+
+        studentNames.add("Alice");
+        studentNames.add("Bob");
+        studentNames.add("Charlie");
+
+        System.out.println("Student at index 1: " + studentNames.get(1));
+        }
+    }
+}
