49b4df44ea9b2f911486578819b62a95018bf7c8
Joshua <joshua.palokaran@gmail.com>
2026-03-12 20:45:50 +0530

    Affine added

--- Information Security/affine.py
+++ Information Security/affine.py
@@ -1,26 +1,27 @@
-def encrypt(text):
+def encrypt(text,a,b):
     result=""
     for x in text:
-        if x.isalpha():
-            shift=(ord(x)-ord("A")+3)%26
-            result+=chr(shift + ord("A"))
-        else:
-            result+=x
+       shift=(a*(ord(x)-ord("A"))+b)%26
+       result+=chr(shift + ord("A"))
     return result
 
-def decrypt(text):
+def decrypt(text,a,b):
     result=""
+    a_inv=pow(a,-1,26)
+
+    for i in range(26):
+        if(a*i)%26==1:
+            a_inv=i
+            break
+
     for x in text:
-        if x.isalpha():
-            shift=(ord(x)-ord("A")-3)%26
-            result+=chr(shift + ord("A"))
-        else:
-            result+=x        
+        shift=a_inv*((ord(x)-ord("A"))-b)%26
+        result+=chr(shift + ord("A"))
     return result
 
 txt=input("Enter the text: ").upper()
-a=int(input("Enter A: "))
-b=int(input("Enter B: "))
-restxt=encrypt(txt)
+# a=int(input("Enter A: "))
+# b=int(input("Enter B: "))
+restxt=encrypt(txt,5,8)
 print(restxt)
-print(decrypt(restxt))
+print(decrypt(restxt,5,8))
